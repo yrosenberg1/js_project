c94e812bf9df744af9d182534b7ba9944b164b7c
Yehudah <jros987@gmail.com>
2021-02-14 23:53:24 -0500

    reverted back

--- src/index.js
+++ src/index.js
@@ -1,103 +1,86 @@
 import { csv, select, scaleLinear, max, scaleBand, axisLeft, axisBottom  } from "d3";
+ 
 import "./styles/index.scss";
 import * as d3 from 'd3';
-import search from "./scripts/search";
+import playerData from "./scripts/search";
+
 import statsTable from './scripts/statstable';
-const svg = select('svg')
 
 
 // document.addEventListener("DOMContentLoaded", () => {
-//     const svg = document.getElementById('svg');
-   
-// })
-
+    //     const svg = document.getElementById('svg');
+    
+    // })
+    
+const svg = select('svg')
 const width = +svg.attr('width');
 const height = +svg.attr('height');
 
-search.then(playerStats =>{
-    debugger
-    console.log("playerStats")
-    console.log(playerStats)
-    render(playerStats)
-})
+const margin = {left: 30, top: 20, right: 100, bottom: 50}
+    const chartWidth = width - margin.left - margin.right;
+    const chartHeight = height - margin.top - margin.bottom;
+
+//     playerData.then(playerStats =>{
+//     debugger
+   
+//     render(playerStats)
+// })
+
 const render = data => {
-    // console.log(data[0])
-    // console.log(Object.keys(data[0]));
-    // const xKeys = Object.keys(data[0])
-    // console.log(xKeys)
-    
-    // const columns = data.columns
-//    const xKeys = data.map(d => d.key)
-//    const yValues = data.map(d => d.value)
+    //  
    const xKeys = d => d.key
    const yValues = d => d.value
    
-
-   
-    const margin = {left: 30, top: 20, right: 100, bottom: 50}
-    const chartWidth = width - margin.left - margin.right;
-    const chartHeight = height - margin.top - margin.bottom;
+    
    
     const xScale = scaleBand()
         .domain(data.map(xKeys))
         .range([0, chartWidth])
         .padding(0.1);
-        console.log("xscale domain")
-        console.log(xScale.domain());
-        console.log("xScale.range")
-        console.log(xScale.range())
-        // console.log("xscale range")
-        // console.log(xScale.range());
-     
+       
+        
    const yScale = scaleLinear()
         .domain([0, 100])
         // .domain([0, max(data, yValues)])
         
         .range([chartHeight, 0])
-        console.log("yscale domain")
-        console.log(yScale.domain());
-        console.log("yscale range")
-        console.log(yScale.range());
-    
-
-        const yAxis = axisLeft(yScale)
-        const xAxis = axisBottom(xScale);
+       
+    const yAxis = axisLeft(yScale)
+    const xAxis = axisBottom(xScale);
+   
     const g = svg.append('g')
          .attr(`transform`, `translate(${margin.left}, ${margin.top})` );
 
-        g.append('g').call(yAxis)
+    g.append('g').call(yAxis)
        
-        g.append('g').call(xAxis)
+    g.append('g').call(xAxis)
             .attr(`transform`, `translate(0, ${chartHeight})` )
        
-        g.selectAll('rect').data(data)
+    g.selectAll('rect').data(data)
             .enter().append('rect')
             .attr('x', d => xScale(xKeys(d)))
             .attr('y', d => yScale(yValues(d)))
             .attr('width', xScale.bandwidth())
             // .attr('height', yScale(xKeys))
-            
             .attr('height', d => chartHeight - yScale(yValues(d)))
 
  }
-      
-      
-      //    columns = d3.map(data, function(d){return(d.group)}).keys()
 
-//  .call(d3.axisBottom(xScale))
 
-//     const yScale = scaleLinear()
-//         .domain([0, max(data, d => d.xba )])
-//         .range([0, width]);
-// console.log(yScale.domain());
-
-// svg.selectAll('rect').data(data)
-//     .enter().append('rect')
-//     .attr(width, 200)
-//     .attr(height, d => yScale())
-
-// }
-
+//  function update(variable) {
+//     d3.csv("/src/dataset/Bref2020_stats.csv", d3.autoType, data => {
+//          const playerArray = [];
+   
+//     const xKeys = ["Games", "Runs Scored", "Hits", "HR", "RBI", "BA", "OBP", "SLG", "OPS", "OPS+"]
+//     for (const key in playerObject){
+//         if (xKeys.includes(key)){
+//         playerArray.push({
+//             key:key,
+//             value: playerObject[key] * 100
+//         })
+//     }}
+//     }
+//     )}
 // d3.csv("/src/dataset/2020statsBaseballSavant.csv",  d3.autoType).then(data => {
 //     // data.forEach(row => {
 //     //     row.year = +row.year,
@@ -109,15 +92,25 @@ const render = data => {
 //     render(data);
 // })
 
-// d3.csv("/src/dataset/Bref2020_stats.csv", d3.autoType).then(data => {
-//     // data.forEach(row => {
-//     //   row.year
+d3.csv("/src/dataset/Bref2020_stats.csv", d3.autoType).then(data => {
+    // data.forEach(row => {
+    //   row.year
     
-        
+    const playerObject = data[0]
+      const playerArray = []
+    //   console.log(playerArray)
+      const xKeys = ["Games", "Runs Scored", "Hits", "HR", "RBI", "BA", "OBP", "SLG", "OPS", "OPS+"]
+      for (const key in playerObject){
+          if (xKeys.includes(key)){
+          playerArray.push({
+              key:key,
+              value: playerObject[key] * 100
+          })
+      }}
    
-//     // })
-//     console.log(data)
-//     render(data);
-// })
+    
+    
+    render(playerArray);
+})
 
 
--- src/scripts/search.js
+++ src/scripts/search.js
@@ -1,82 +1,118 @@
 import * as d3 from 'd3';
-
+// const searchBar = document.querySelector("input");
+// let players = [];
+// //Event listener to register user input
 //Search function
-
-const search = d3.csv("/src/dataset/Bref2020_stats.csv", d3.autoType).then(data => {
-    
-const searchBar = document.querySelector("input");
-
-//Event listener to register user input
-
-
-searchBar.addEventListener('keyup', e => {
+const searchBar = document.getElementById('searchBar');
+let playerObjects = []
+const playerData = d3.csv("/src/dataset/Bref2020_stats.csv", d3.autoType).then(data => {
+        
+    return data
+}).then( players => {
+    const  playersNames = players.map(player => player.Name.toLowerCase())
+console.log("playersNames:")
+console.log(playersNames)
+    searchBar.addEventListener('keyup', e => {
+      
     const input = e.target.value.toLowerCase();
     console.log(input)
-    const filtered = playersNames.filter(name => name.toLowerCase().includes(input))
-    //     if (filtered.length === 1){
-    //   const  selectedName = filtered[0]
-    // }
-console.log(filtered)
-})
-
-
- 
-    const playersNames = data.map(players => players.Name)
-    console.log(playersNames)
-    // let selectedName = "";
-
-    // if (filtered.length === 1){
-    //     selectedName = filtered[0]
-    // }
-     const selectedName = "José Abreu"
-  const playerObject = selectedName ? data.find(player => player.Name === selectedName ) : ""
-        const playerArray = []
-        console.log(playerArray)
-        document.getElementById("percentage-button").addEventListener("click", percentageFunction);
-        document.getElementById("ranking-button").addEventListener("click", rankingFunction);
-
-        const percentageFunction = () =>{ xKeysPercentile = ["Games", "Runs Scored", "Hits", "HR", "RBI", "BA", "OBP", "SLG", "OPS", "OPS+"]
+    const filteredPlayers = playersNames.filter(name => name.toLowerCase().includes(input) 
+    );
         
-        for (const key in playerObject){
-            if (xKeysPercentile.includes(key)){
-            playerArray.push({
-                key:key,
-                value: playerObject[key] * 100
-            })
-        }}
-        return playerArray
+    if (filteredPlayers.length < 10){
+        filteredPlayers.forEach(playerName => {  
+          const findPlayer =  players.find(player => { return  player.Name.toLowerCase() === playerName})
+          console.log(findPlayer)
+          if (!playerObjects.includes(findPlayer)){
+              playerObjects.push(findPlayer)
+          }
+        } )
+        
+
+        
+        // const playerObject = players.find(player => player.Name === filteredPlayers[0])
+        // const playerList = d3.select("search-bar-results")
+          
+        // playerList.selectAll('li').data(playerObjects)
+        
+        //     .enter().append('li')
+            
+        //     // .text(d => d.Name );
+        //     debugger
+
+        playerObjects.forEach( playerObject => { 
+             const playerList = document.getElementById("search-bar-results")
+            const playerli = document.createElement("li")
+            playerli.setAttribute("class", "search-list")
+            playerli.innerText = playerObject.Name
+            playerList.appendChild(playerli)
+            
+         })
+
+       
+        // return playerObject
     }
+    const random = Math.floor(Math.random() * playerObjects.length);
 
-        const rankingFunction = () =>{
-        const xKeysRankings = ["Games Ranking","Runs Ranking", "Hits Ranking","HR Ranking", "RBI Ranking", "BA Ranking", "OBP Ranking", "SLG Ranking", "OPS Ranking", "OPS+ Ranking"]
-        for (const key in playerObject){
-            if (xKeysRankings.includes(key)){
-            playerArray.push({
-                key:key,
-                value: playerObject[key] * 100
-            })
-        }}
-        return playerArray}
-    //     if (playerNames.includes(input)){
-    //        playerName = input
-    //    }
+   const playerObject = playerObjects[random]
+   console.log("playerObject")   
+   console.log(playerObject)   
+   return playerObject
+});
+
+})
+//     console.log(input)
+//     const filtered = playersNames.filter(name => name.toLowerCase().includes(input))
+//     //     if (filtered.length === 1){
+//     //   const  selectedName = filtered[0]
+//     // }
+// // console.log(filtered)
+//  if (filtered.length < 10){
+//      const list = 
+//  }
+// debugger
+// })
+// console.log("searchResult:")
+// console.log(searchResult)
+ 
+//     const playersNames = data.map(players => players.Name)
+//     console.log(playersNames)
+//     // let selectedName = "";
+
+//     // if (filtered.length === 1){
+//     //     selectedName = filtered[0]
+//     // }
+//      const selectedName = "José Abreu"
+//   const playerObject = selectedName ? data.find(player => player.Name === selectedName ) : ""
+// //   const playerArray = []
+// //   console.log(playerArray)
+// //   const xKeys = ["Games", "Runs Scored", "Hits", "HR", "RBI", "BA", "OBP", "SLG", "OPS", "OPS+"]
+// //   for (const key in playerObject){
+// //       if (xKeys.includes(key)){
+// //       playerArray.push({
+// //           key:key,
+// //           value: playerObject[key] * 100
+// //       })
+// //   }}
+//     //     if (playerNames.includes(input)){
+//     //        playerName = input
+//     //    }
     
     
-        // console.log(playerObject)
-        //    console.log(playerArray) 
+//         // console.log(playerObject)
+//         //    console.log(playerArray) 
        
        
         
     
     
-debugger  
-  // render(playerArray);
-    return playerArray
     
-})
+//     // render(playerArray);
+//     return playerArray
+// })
 
 
 
 
 
-export default search; 
+export default playerData; 
